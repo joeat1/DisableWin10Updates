581f0f5e55e3103413300219127ef39350e81676
Joe <34472403+joeat1@users.noreply.github.com>
2019-11-16 14:36:05 +0800

    Update DisableUpdates.bat

--- DisableUpdates.bat
+++ DisableUpdates.bat
@@ -51,6 +51,16 @@ schtasks /delete /tn "\Microsoft\Windows\UNP\RunUpdateNotificationMgr" /f 2>nul
 
 echo Editing Registry
 
+REG add "HKLM\SYSTEM\CurrentControlSet\Services\UsoSvc" /v "Start" /t REG_DWORD /d "4" /f
+REG add "HKLM\SYSTEM\CurrentControlSet\Services\wuauserv" /v "Start" /t REG_DWORD /d "4" /f
+REG add "\HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v "Start" /t REG_DWORD /d "4" /f
+REG add "\HKLM\SYSTEM\CurrentControlSet\Services\WpnService" /v "Start" /t REG_DWORD /d "4" /f
+REG add "\HKLM\SYSTEM\CurrentControlSet\Services\WpnUserService" /v "Start" /t REG_DWORD /d "4" /f
+REG add "\HKLM\SYSTEM\CurrentControlSet\Services\BITS" /v "Start" /t REG_DWORD /d "4" /f
+
+REG add "HKLM\SYSTEM\CurrentControlSet\Services\wuauserv" /v "FailureActions" /t REG_BINARY /d "80510100000000000000000003000000140000000000000060ea00000000000060ea00000000000000000000" /f
+REG add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v "FailureActions" /t REG_BINARY /d "840300000000000000000000030000001400000000000000c0d4010000000000e09304000000000000000000" /f
+REG add "HKLM\SYSTEM\CurrentControlSet\Services\UsoSvc" /v "FailureActions" /t REG_BINARY /d "805101000000000000000000030000001400000000000000c0d4010000000000e09304000000000000000000" /f
 
 
 echo  All Done!
